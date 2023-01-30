76cbfa1b3a68771bd8e06c9fe5d13e67302652c4
hector918 <hector_zhong@outlook.com>
2023-01-30 17:55:13 -0500

    back end init

--- queries/snacks.js
+++ queries/snacks.js
@@ -1,3 +1,28 @@
 const db = require("../db/dbConfig.js");
 
-module.exports = {};
+const getAllSnacks = async (id) => {
+  try {
+    const allSnacks = await db.any(`SELECT * FROM snacks ORDER BY id ASC`,id);
+    return allSnacks;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteSnack = async ( id ) => {
+  try {
+    const snack = await db.one(`DELETE FROM snacks WHERE id = $[id] RETURNING *`, { id });
+  } catch (error) {
+    throw error;
+  }
+}
+
+const createSnack = async ( body ) =>{
+  try {
+    const snack = await db.one(`INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES() RETURNING *`,
+    body);
+  } catch (error) {
+    throw error;
+  }
+}
+module.exports = {getAllSnacks};
